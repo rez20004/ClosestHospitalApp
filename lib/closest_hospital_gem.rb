69f00e6d6236d19996501244398474c252574851
Adebamigbe <adebamigbe.fasanmade@gmail.com>
2013-11-29 13:11:14 +0000

    hospital gem

--- lib/closest_hospital_gem.rb
+++ lib/closest_hospital_gem.rb
@@ -1,5 +1,30 @@
 require "closest_hospital_gem/version"
+require 'net/http'
+require 'httparty'
 
 module ClosestHospitalGem
-  # Your code goes here...
+  def self.search lat, lon
+    url = URI.parse(URI.encode("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{lon}&types=hospital|health|doctor&sensor=false&rankby=distance&key=AIzaSyDG1sETP6oYhAU4GFYZO8kZRH28KiffPlU"))
+    request = Net::HTTP::Get.new(url.path)
+
+    response = Net::HTTP.start(url.host, url.port, use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
+      http.request(request)
+    end
+
+    puts response.body
+  end
+
+     options = {
+        :location => location,
+        :name => name
+
+    }
+
+    if types
+      types = (types.is_a?(Array) ? types.join('|') : types)
+      options.merge!(:types => types)
+    end
+
+    mashup(self.class.get("/search/json", :query => options.merge(self.default_options)))
+  end
 end
